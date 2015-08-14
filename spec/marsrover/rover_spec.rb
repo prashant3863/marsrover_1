9e747edb70164965a17d96fd7a8197248a131e04
Prashant Mittal <TheWhip@Prashants-MacBook-Pro.local>
2015-08-14 11:30:59 +0530

    refactored spec for context

--- spec/marsrover/rover_spec.rb
+++ spec/marsrover/rover_spec.rb
@@ -1,15 +1,16 @@
 require "spec_helper"
 
 module MarsRover
-  describe "Rover" do 
-    it "turns 90 deg. to the left when left is called" do
-      rover = Rover.new(1, 2, "N")
-      expect(rover.left).to eq("W")
-    end
+  describe "Rover" do
+  let(:rover) { Rover.new(1, 2, "N") } 
+    context "Turning" do 
+      it "turns 90 deg. to the left when left is called" do
+        expect(rover.left).to eq("W")
+      end
 
-    it "turns 90 deg. to the left when left is called" do
-      rover = Rover.new(1, 2, "N")
-      expect(rover.right).to eq("E")
+      it "turns 90 deg. to the left when left is called" do
+        expect(rover.right).to eq("E")
+      end
     end
   end
 end
